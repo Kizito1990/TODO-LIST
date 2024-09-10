dafc4226ba1f782f0c65da7322e58666c6947fb1
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2024-09-10 10:07:43 +0100

    added Login and Update functionalities

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .import views
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,DeleteView,CustomLoginView
 
 urlpatterns = [
-    path('',views.tasklist, name = 'task'),
+    path('login/', CustomLoginView.as_View(), name = 'login'),
+    path('',TaskList.as_view(), name = 'tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name = 'task'),
+    path('task_create/', TaskCreate.as_view(), name = 'task_create'),
+    path('task_update/<int:pk>/', TaskUpdate.as_view(), name = 'task_update'),
+    path('task_delete/<int:pk>/', DeleteView.as_view(), name = 'task_delete'),
+
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.Views import LoginView
+from .models import Tasks
 
 # Create your views here.
-def tasklist(request):
-    return HttpResponse('Hello World')
+
+class CustomLoginView(LoginView):
+    template_name = 'todolist/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def success_url(self):
+        return reverse_lazy('tasks')
+class TaskList(ListView):
+    model = Tasks
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Tasks
+    context_object_name = 'task'
+    template_name = 'todolist/task.html'
+
+class TaskCreate(CreateView):
+    model = Tasks
+    template_name = 'todolist/task_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Tasks
+    template_name = 'todolist/task_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Tasks
+    template_name = 'todolist/tasks_comfirm_delete.html'
+    context_object_name = 'tasks'
+    success_url = reverse_lazy('tasks')
